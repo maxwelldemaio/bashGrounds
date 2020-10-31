c7ec174457feb9aafc695615d6deb394bc9e996c
Maxwell DeMaio <maxdman321@gmail.com>
2020-10-31 22:50:22 +0000

    created subdir for .sh files

---
+++ bashFiles/math.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Practice simple math expressions
+
+expr 5 + 2
+expr 5 - 2
+expr 5 \* 2
+# Note integer division rounds down
+expr 5 / 2
+expr 40 % 21
+
+# Using the bench calculator
+echo "4.38 * 26.73" | bc -l
+
