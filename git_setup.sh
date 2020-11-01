6da1de314b04619f91a42e815b918050f61a83b1
maxwelldemaio <maxdman321@gmail.com>
2020-11-01 14:51:34 +0000

    removed test

--- git_setup.sh
+++ git_setup.sh
@@ -6,5 +6,7 @@ read git_email
 
 git config --global user.email $git_email
 git config --global user.name $git_user
+# Default 900 seconds to store password
+git config credential.helper cache
 
 echo "Github username and email are now configured!"
