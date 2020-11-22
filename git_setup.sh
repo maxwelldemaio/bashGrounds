1487cb318f4b78ccce21d45d059946f04829c8ee
maxwelldemaio <maxdman321@gmail.com>
2020-11-22 20:50:44 +0000

    Set credential cache to 1 hour

--- git_setup.sh
+++ git_setup.sh
@@ -6,7 +6,9 @@ read git_email
 
 git config --global user.email $git_email
 git config --global user.name $git_user
-# Default 900 seconds to store password
-git config credential.helper cache
+
+# Change from default 900 seconds to store password to 1 hour
+git config --global credential.helper 'cache --timeout=3600'
 
 echo "Github username and email are now configured!"
+echo "Github credential cache set to 1 hour"
