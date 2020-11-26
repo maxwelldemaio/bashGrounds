4a0aadcf5dea756e8e764c52ac86821a673e0aed
maxwelldemaio <maxdman321@gmail.com>
2020-11-26 04:17:08 +0000

    Created private dotfile and worked on install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 ## Symlinks
 
-if [ "$#" -ne 1]; then
+if [ "$#" -ne 1 ]; then
 	echo "Usage: install.sh <home_directory>"
 	exit 1
 fi
@@ -14,4 +14,19 @@ homedir=$1
 
 # Dotfiles directory
 dotfiledir=${homedir}/bashGrounds
+
+# List of files/folders to symlink in ${homedir}
+# (If adding more, delimit string with spaces
+files="bash_profile"
+
+# Change to the dotfiles directory
+echo "Changing to the ${dotfiledir} directory"
+cd ${dotfiledir}
+echo "...done"
+
+# Create symlinks (will overwrite old dotfiles)
+for file in ${files}; do
+	echo "Creating symlink to $file in home directory."
+	ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
+done
 
